34b3ea8ca029ad158b8281ae14de41695ed9b69a
ritin0204 <ritintiwari417@gmail.com>
2023-06-21 21:01:19 +0530

    completed code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './Styles/App.css';
+
+const makeTitleCase = (str) => {
+  const words = str.split(' ');
+  const titleCaseWords = words.map(word => {
+    const firstLetter = word[0].toUpperCase();
+    const restOfWord = word.slice(1).toLowerCase();
+    return firstLetter + restOfWord;
+  });
+  return titleCaseWords.join(' ');
+};
+
 
 function App() {
+  const [quote, setQuote] = useState({ text: 'The truth will set you free.', author: 'The Bible' });
+
+  const generateQuote = () => {
+    fetch('https://type.fit/api/quotes')
+      .then(response => response.json())
+      .then(data => {
+        const rndmIndex = Math.floor(Math.random() * data.length);
+        setQuote(data[rndmIndex]);
+      });
+  };
+
+  useEffect(() => {
+    generateQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="quote-box">
+        <p className='card-text'>
+          <q>
+            {makeTitleCase(quote.text)}
+          </q>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className='card-footer'>~ 
+          {quote.author ? makeTitleCase(quote.author) : 'Anonymous'}
+        </p>
+        <button onClick={generateQuote}>Get New</button>
+      </div>
     </div>
   );
 }
